2888a76d04b575cf2e39ba8f0e84ac967e576f99
Mickael Maison <mickael.maison@gmail.com>
2013-03-13 21:41:58 +0000

    Changed updateURL and downloadURL
    They both point to userscripts.org now

--- gm_imdb_charts.meta.js
+++ gm_imdb_charts.meta.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name            IMDb Top 250 & Bottom 100
-// @version         2.0
+// @version         2.1
 // @description     Keep track of the movies you've seen in the IMDb Top 250 and Bottom 100 !
 // @namespace       https://github.com/mimaison/gm_imdb_charts
 // @grant           none
-// @updateURL       https://github.com/mimaison/gm_imdb_charts/blob/master/gm_imdb_top250.meta.js
-// @downloadURL     https://github.com/mimaison/gm_imdb_charts/blob/master/gm_imdb_top250.user.js
+// @updateURL       http://userscripts.org/scripts/source/153904.meta.js
+// @downloadURL     http://userscripts.org/scripts/source/153904.user.js
 // @match           http://www.imdb.com/chart/top*
 // @match           http://www.imdb.com/chart/bottom*
 // ==/UserScript==
